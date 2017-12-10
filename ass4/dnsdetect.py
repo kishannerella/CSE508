c26a33704e3f2203b168d6e461270f8259afc3b5
Kishan Nerella <kishannerella@gmail.com>
2017-12-10 03:34:59 -0500

    remove duplicates

--- ass4/dnsdetect.py
+++ ass4/dnsdetect.py
@@ -9,31 +9,46 @@ data = deque(maxlen=100)
 def intersection(pkt1, pkt2):
 	for i in range(0, pkt1[DNS].ancount):
 		for j in range(0, pkt2[DNS].ancount):
-			if (pkt1[DNSRR][i].rdata == pkt2[DNSRR][j].rdata):
+			if (pkt1[DNSRR][i].type == 1 and pkt2[DNSRR][j].type == 1 and
+				pkt1[DNSRR][i].rdata == pkt2[DNSRR][j].rdata):
 				return True
 	return False;
 
 def printPackets(pkt1, pkt2):
-	print datetime.datetime.fromtimestamp(pkt1.time).strftime("%Y%m%d-%H:%M:%S.%f"),
-	print " DNS poisoning attempt"
+
 	l1 = []
 	for i in range(0, pkt1[DNS].ancount):
-		l1.append(pkt1[DNSRR][i].rdata)
+		if (pkt1[DNSRR][i].type == 1):
+			l1.append(pkt1[DNSRR][i].rdata)
 
 	l2 = []
 	for i in range(0, pkt2[DNS].ancount):
-		l2.append(pkt2[DNSRR][i].rdata)
+		if (pkt2[DNSRR][i].type == 1):
+			l2.append(pkt2[DNSRR][i].rdata)
+
+   	if len(l1) == 0 and len(l2) == 0:
+   		return
+
+	print datetime.datetime.fromtimestamp(pkt1.time).strftime("%Y%m%d-%H:%M:%S.%f"),
+	print " DNS poisoning attempt"
 	print "TXID " + format(pkt1[DNS].id, "X") + \
 	      "  Request " + pkt1[DNSQR].qname
 	print "Answer1  " + str(l1)
 	print "Answer2  " + str(l2)
+	print "\n\n"
 
+def insert(pkt):
+	for prevPkt in data:
+		if (prevPkt[DNS].id == pkt[DNS].id):
+			return;
+
+	data.append(pkt)
 
 def detect(pkt):
 	if (pkt.haslayer(DNSQR) and
 		pkt[DNS].qr == 1 and 
 		pkt[DNSQR].qtype == 1):
-		print 
+		#print data
 		for prevPkt in data:
 			if (pkt[DNS].id == prevPkt[DNS].id and
 				pkt[IP].dst == prevPkt[IP].dst and
@@ -45,7 +60,7 @@ def detect(pkt):
 					pkt[Ether].src != prevPkt[Ether].src):
 					printPackets(pkt, prevPkt)
 		
-		data.append(pkt)
+		insert(pkt)
 
 parser = argparse.ArgumentParser(description="dnsdetect", add_help=False)
 parser.add_argument("-i", help="interface")
@@ -60,6 +75,7 @@ if args.expression:
 	exp = " ".join(args.expression)
 	bpf = bpf + " and " + "(" + exp + ")" 
 #print bpf
+print args.r
 if args.i is None:
 	if args.r is None:
 		sniff(filter=bpf, prn=detect)
